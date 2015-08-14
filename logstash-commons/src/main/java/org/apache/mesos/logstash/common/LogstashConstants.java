67959e383763f18bd04f47e9a912cb5f3f09f31f
Drew Robb <drewrobb@gmail.com>
2015-08-13 23:58:49 -0700

    Add redis, change host

--- logstash-commons/src/main/java/org/apache/mesos/logstash/common/LogstashConstants.java
+++ logstash-commons/src/main/java/org/apache/mesos/logstash/common/LogstashConstants.java
@@ -3,8 +3,8 @@ package org.apache.mesos.logstash.common;
 public interface LogstashConstants {
     String FRAMEWORK_NAME = "logstash";
     int FAILOVER_TIMEOUT = 300000;
-    String EXECUTOR_IMAGE_NAME = "mesos/logstash-executor";
-    String EXECUTOR_IMAGE_TAG = "0.0.6";
+    String EXECUTOR_IMAGE_NAME = "docker.strava.com/mesos/logstash-executor";
+    String EXECUTOR_IMAGE_TAG = "latest";
     String EXECUTOR_IMAGE_NAME_WITH_TAG = EXECUTOR_IMAGE_NAME + ":" + EXECUTOR_IMAGE_TAG;
     String NODE_NAME = "logstash.node";
     String TASK_NAME = "logstash.task";
